5bee711516ce384f5aa56aea36d6fd0708cf12ea
sujauddoula352 <sujauddoulasohel352@gmail.com>
2024-11-04 23:20:44 +0600

    Add success toast notification for job posting confirmation

--- src/pages/addJob/AddJob.jsx
+++ src/pages/addJob/AddJob.jsx
@@ -1,9 +1,51 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { AuthContext } from "../../provider/AuthProvider";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 const AddJob = () => {
   const [startDate, setStartDate] = useState(new Date());
+  const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleFromSubmission = async (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const job_title = form.job_title.value;
+    const min_price = parseFloat(form.min_price.value);
+    const max_price = parseFloat(form.max_price.value);
+    const category = form.category.value;
+    const deadline = form.deadline.value;
+    const description = form.description.value;
+    const email = user?.email;
+    const bidData = {
+      job_title,
+      email,
+      deadline,
+      category,
+      min_price,
+      max_price,
+      description,
+      buyer: {
+        email,
+        name: user?.DisplayName,
+        photo: user?.photoURL,
+      },
+    };
+    try {
+      const data = await axios.post(
+        `${import.meta.env.VITE_API_URL}/job`,
+        bidData
+      );
+      console.log(data);
+      toast.success("Job Post successfully!");
+    } catch (err) {
+      console.log(err);
+    }
+    console.table(bidData);
+  };
   return (
     <div className="flex justify-center items-center min-h-[calc(100vh-306px)] my-12">
       <section className=" p-2 md:p-6 mx-auto bg-white rounded-md shadow-md ">
@@ -11,7 +53,7 @@ const AddJob = () => {
           Post a Job
         </h2>
 
-        <form>
+        <form onSubmit={handleFromSubmission}>
           <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
             <div>
               <label className="text-gray-700 " htmlFor="job_title">
@@ -33,6 +75,7 @@ const AddJob = () => {
                 id="emailAddress"
                 type="email"
                 name="email"
+                defaultValue={user?.email}
                 disabled
                 className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring"
               />
